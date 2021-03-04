2ee800cf399e328eb448f0d0e81c4229cbadc5ab
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-03-04 19:51:09 +0200

    changed theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
         short_name: `Ammar`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#381696`,
+        theme_color: `#fff`,
         display: `standalone`,
         icon: "src/images/icon.png",
       },

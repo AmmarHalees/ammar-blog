531103e7eb7bd8832341c528cf14488d102970df
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-03-04 21:52:32 +0200

    added gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,14 +42,36 @@ module.exports = {
         }],
       },
     },
+
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "G-W3S5P02VYP",
-        head: true,
-      }
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-W3S5P02VYP", // Google Analytics / GA
+           //"AW-CONVERSION_ID", Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
     },
+
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {

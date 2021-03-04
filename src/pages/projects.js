6b555d16505b60ab14f7f72a5e725cf1de433659
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-03-04 19:23:33 +0200

    PROJ CARD

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,7 @@ import Helmet from "react-helmet"
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 import PageHero from "../components/PageHero"
+import HomeSection from "../components/HomeSection"
 
 const Projectspage = ({
   data: {
@@ -11,15 +12,112 @@ const Projectspage = ({
 }) => {
   return (
     <Layout>
-      <Helmet htmlAttributes={{lang:'en'}}>
+      <Helmet htmlAttributes={{ lang: 'en' }}>
         <title>Projects - {site.siteMetadata.title}</title>
         <meta name="description" content={"Projects page of " + site.siteMetadata.title} />
       </Helmet>
 
-      <PageHero title="Projects" subtitle="Open source contributions & code templates." links={null}/>
+      <PageHero title="Projects" subtitle="Open source contributions & code templates." links={null} />
 
-        <br/>
-        <p>Under construction</p>
+
+      <HomeSection title="'For fun' projects" >
+        <div className="grids">
+     
+          <div className="_project_card ">
+
+            <h2>
+
+              Social Feed
+
+            </h2>
+
+            <h3>
+
+              <span className="_tech-item">React </span>
+              <span className="_tech-item">Unsplash API </span>
+              <span className="_tech-item">Redux </span>
+              <span className="_tech-item">React Router </span>
+     
+
+            </h3>
+
+            <div className="_controls-container">
+
+              <a href="https://mosaw.netlify.app" title="unsplash api" target="_blank" rel="noreferrer noopener">Demo</a>
+
+              <a href="https://mosaw.netlify.app" title="unsplash api" target="_blank" rel="noreferrer noopener">----</a>
+
+
+            </div>
+
+
+          </div>
+
+     
+          <div className="_project_card ">
+
+            <h2>
+
+              Twitter Feed
+
+            </h2>
+
+            <h3>
+
+              <span className="_tech-item">React </span>
+              <span className="_tech-item">React Router </span>
+     
+
+            </h3>
+
+            <div className="_controls-container">
+
+              <a href="https://ammarhalees.github.io/twitter-feed/#/" title=" Twitter Feed" target="_blank" rel="noreferrer noopener">Demo</a>
+
+              <a href="https://github.com/AmmarHalees/twitter-feed" title=" Twitter Feed source code" target="_blank" rel="noreferrer noopener">Source</a>
+
+
+            </div>
+
+
+          </div>
+
+     
+          <div className="_project_card ">
+
+            <h2>
+
+              Social Feed
+
+            </h2>
+
+            <h3>
+
+              <span className="_tech-item">React </span>
+              <span className="_tech-item">Unsplash API </span>
+              <span className="_tech-item">Redux </span>
+              <span className="_tech-item">React Router </span>
+     
+
+            </h3>
+
+            <div className="_controls-container">
+
+              <a href="https://mosaw.netlify.app" title="unsplash api" target="_blank" rel="noreferrer noopener">Demo</a>
+
+              <a href="https://mosaw.netlify.app" title="unsplash api" target="_blank" rel="noreferrer noopener">----</a>
+
+
+            </div>
+
+
+          </div>
+
+
+
+        </div>
+      </HomeSection>
+
 
     </Layout>
   )

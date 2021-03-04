857ce5933df8ea8dd954e29d78343ba3c0151096
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-03-04 19:41:22 +0200

    added more projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -20,7 +20,7 @@ const Projectspage = ({
       <PageHero title="Projects" subtitle="Open source contributions & code templates." links={null} />
 
 
-      <HomeSection title="'For fun' projects" >
+      <HomeSection title="For fun" >
         <div className="grids">
      
           <div className="_project_card ">
@@ -87,25 +87,57 @@ const Projectspage = ({
 
             <h2>
 
-              Social Feed
+              react-bna : <span className="_subtitle">before and after image slider component</span>
 
             </h2>
 
             <h3>
 
-              <span className="_tech-item">React </span>
-              <span className="_tech-item">Unsplash API </span>
-              <span className="_tech-item">Redux </span>
-              <span className="_tech-item">React Router </span>
-     
+              <span className="_tech-item">Babel 6.x.x
+ </span>
+              <span className="_tech-item">Webpack 4.x.x </span>
 
+              <span className="_tech-item">ES6+
+
+ </span>
             </h3>
 
             <div className="_controls-container">
 
-              <a href="https://mosaw.netlify.app" title="unsplash api" target="_blank" rel="noreferrer noopener">Demo</a>
+              <a href="https://sliderbna.netlify.app/" title="Slider before and after" target="_blank" rel="noreferrer noopener">Demo</a>
 
-              <a href="https://mosaw.netlify.app" title="unsplash api" target="_blank" rel="noreferrer noopener">----</a>
+              <a href="https://github.com/AmmarHalees/react-bna" title="Slider before and after" target="_blank" rel="noreferrer noopener">Source</a>
+
+
+            </div>
+
+
+          </div>
+
+     
+          <div className="_project_card ">
+
+            <h2>
+
+            Hooks form validation 
+
+            </h2>
+
+            <h3>
+
+              <span className="_tech-item">React</span>
+         
+
+              <span className="_tech-item">ES6+
+
+ </span>
+            </h3>
+
+            <div className="_controls-container">
+
+              <a href="https://ammarhalees.github.io/hooks-form-website/" title="Hooks form validation" target="_blank" rel="noreferrer noopener">Demo</a>
+
+              <a href="https://github.com/AmmarHalees/hooks-form-website/blob/master/src/utils/validator.js" title="Hooks form validation source" target="_blank" rel="noreferrer noopener">Source</a>
 
 
             </div>

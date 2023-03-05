76f40bc756c62f4cf44703ae077aee5732394112
Ammar Halees <haleesammar@gmail.com>
2023-03-05 22:24:27 +0300

    stopped projects

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import {Link} from "gatsby"
 export default (props) => (
   <nav className="navigation"> 
     <Link to="/">Home</Link>
-    <Link to="/projects">Projects</Link>
+    {/* <Link to="/projects">Projects</Link> */}
     <Link to="/blog">Blog</Link>
     <Link to="/talks">Talks</Link>
 
